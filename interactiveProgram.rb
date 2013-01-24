fc281215847c6c66fe1d8eb8d1834944b56a2449
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-24 15:09:14 +0530

    first

---
+++ interactiveProgram.rb
@@ -0,0 +1,14 @@
+evaluate_code = ""
+until (next_line = gets).chomp =~ /^[q]$/i
+  if next_line.length == 1
+      puts eval(evaluate_code)
+      evaluate_code = ""
+  else
+    begin
+      eval(evaluate_code + next_line)
+      evaluate_code += next_line
+    rescue Exception => e
+      puts e.message
+    end
+  end
+end
