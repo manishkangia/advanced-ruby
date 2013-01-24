77dcae0912dd8a82f218f328523f0189fa26e222
ShubhamGupta <shubham.gupta@vinsol.com>
2013-01-24 17:34:22 +0530

    fix

--- interactiveProgram.rb
+++ interactiveProgram.rb
@@ -3,8 +3,8 @@ until ( next_line = gets ).chomp =~ /^[q]$/i
   if( next_line.length == 1 )
     begin
       puts eval( evaluate_code )
-      rescue Exception => e
-        puts e.message
+    rescue Exception => e
+      puts e.message
     end
     evaluate_code = ""
   else
