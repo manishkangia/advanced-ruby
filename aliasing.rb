eb1c86e7d31f71bcc5d318ac3a2854857f015617
ShubhamGupta <shubham.gupta@vinsol.com>
2013-02-06 11:04:39 +0530

    with!

--- aliasing.rb
+++ aliasing.rb
@@ -1,56 +1,49 @@
-module MyModule
-  
-  module ClassMethods
+module AliasModule
+  def chained_aliasing( func_name, extend_with )
+    ends_with = (/[?=!]$/).match( func_name ) 
     
-    def chained_aliasing( func_name, extend_with )
-      ends_with = (/[?=!]$/).match( func_name ) 
-      existing_func_name = :"#{func_name}_with_#{extend_with}#{ends_with}"
+    if self.public_instance_methods.include?(func_name) then scope = :public
+    elsif self.protected_instance_methods.include?(func_name) then scope = :protected
+    else scope = :private
+    end
+    
+    func_name_without_end = func_name.to_s.delete(ends_with.to_s).to_sym if ends_with
+    
+    puts "scope of function is : #{scope}"
+    class_eval %{
+      #{scope}
+      alias_method :original_method, :#{func_name}
       
-      if self.public_instance_methods.include?(existing_func_name) then scope = :public
-      elsif self.protected_instance_methods.include?(existing_func_name) then scope = :protected
-      else scope = :private
+      def #{func_name_without_end}_with_#{extend_with}#{ends_with}
+        puts '--logging start'
+        original_method
+        puts "--logging end"
+        puts
       end
       
-      class_eval %{
-        #{scope}
-        alias_method :original_method, :#{func_name}
-        
-        def #{func_name}_with_#{extend_with}#{ends_with}
-          puts '--logging start'
-          original_method
-          puts "--logging end"
-          puts
-        end
-        
-        alias_method :#{func_name}, :#{func_name}_with_#{extend_with}#{ends_with}
+      alias_method :#{func_name}, :#{func_name_without_end}_with_#{extend_with}#{ends_with}
 
-        def #{func_name}_without_#{extend_with}#{ends_with} 
-          original_method
-        end
-      }
-    end
+      def #{func_name_without_end}_without_#{extend_with}#{ends_with} 
+        original_method
+      end
+    }
   end
-
-  def self.included(cls)
-    cls.extend ClassMethods
-  end
-
 end
 
 class Hello
   
-  include MyModule
+  extend AliasModule
   
   protected
   
-  def greet
+  def greet!
     puts "hello boy!"
   end
 
-  chained_aliasing :greet, :logger
+  chained_aliasing :greet!, :logger
 end
 
 say = Hello.new
-say.send :greet
-say.send :greet_with_logger
-say.send :greet_without_logger
+say.send :greet!
+say.send :greet_with_logger!
+say.send :greet_without_logger!
